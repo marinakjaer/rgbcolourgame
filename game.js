6602623c66cfe1df132f391fd014a8530e575cc9
Marina Kjaer <marinaillumkjaer@gmail.com>
2019-04-21 14:29:39 +0200

    assign colours from array to squares

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-alert("Connected");
+let colours = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(255, 0, 255)", "rgb(0, 0, 255)"]
+
+const squares = document.querySelectorAll(".square");
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colours[i];
+}
