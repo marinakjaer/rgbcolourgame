765bd251a5be5b4a17a273d57e634dcb6dc3935b
Marina Kjaer <marinaillumkjaer@gmail.com>
2019-04-21 14:35:09 +0200

    display chosen colour in title

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 let colours = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(255, 0, 255)", "rgb(0, 0, 255)"]
 
 const squares = document.querySelectorAll(".square");
+let chosenColour = colours[3];
+let displayColour = document.getElementById('displayColour');
+
+displayColour.textContent = chosenColour;
 
 for (let i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colours[i];
