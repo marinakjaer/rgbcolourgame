6a09ff5a74f117c0367d944998ae4bd79624e1bf
Marina Kjaer <marinaillumkjaer@gmail.com>
2019-04-21 15:12:26 +0200

    winning colour is now random

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let colours = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(255, 0, 255)", "rgb(0, 0, 255)"]
 
 const squares = document.querySelectorAll(".square");
-let winningColour = colours[3];
+let winningColour = pickColour();
 const displayColour = document.getElementById('displayColour');
 const message = document.getElementById('message');
 
@@ -32,4 +32,9 @@ function changeAllSquaresToWinningColour(colour) {
   for (var i = squares.length - 1; i >= 0; i--) {
     squares[i].style.backgroundColor = colour;
   }
+}
+
+function pickColour() {
+  let random = Math.floor(Math.random() * colours.length)
+  return colours[random];
 }
