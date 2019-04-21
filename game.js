0b6054888858f5058e62a90ceb0e23ebae032f71
Marina Kjaer <marinaillumkjaer@gmail.com>
2019-04-21 14:42:41 +0200

    squares disappear when youre wrong,  youre alerted if correct

--- game.js
+++ game.js
@@ -1,11 +1,24 @@
 let colours = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(255, 0, 255)", "rgb(0, 0, 255)"]
 
 const squares = document.querySelectorAll(".square");
-let chosenColour = colours[3];
+let winningColour = colours[3];
 let displayColour = document.getElementById('displayColour');
 
-displayColour.textContent = chosenColour;
+displayColour.textContent = winningColour;
 
 for (let i = 0; i < squares.length; i++) {
+  // add initial colours to squares
   squares[i].style.backgroundColor = colours[i];
+  // add event listeners to squares when clicked
+  squares[i].addEventListener("click", function(){
+    // get the colour of the square
+    let clickedColour = this.style.backgroundColor;
+    // compare the colour to the chosen colour
+    if(clickedColour === winningColour){
+      alert("you win");
+    } else {
+      this.style.backgroundColor = "#232323";
+    }
+
+  })
 }
